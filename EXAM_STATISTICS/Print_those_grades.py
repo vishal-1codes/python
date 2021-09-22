1d369ed1b80925bc34cd18f29427955c779a69e5
vishal-1codes <pawarvishal10k@gmail.com>
2021-09-22 11:27:50 +0530

    Adding EXAM_STATISTICS to python git repo

---
+++ EXAM_STATISTICS/Print_those_grades.py
@@ -0,0 +1,8 @@
+#Here we simply print all grades of list in separate line
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+  for grade in grades_input:
+    print(grade)
+
+print_grades(grades)
---
+++ EXAM_STATISTICS/The_Variance.py
@@ -0,0 +1,26 @@
+#Here we return variance of list
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+  for grade in grades_input:
+    print(grade)
+
+def grades_sum(scores):
+  total = 0
+  for score in scores: 
+    total += score
+  return total
+    
+def grades_average(grades_input):
+  sum_of_grades = grades_sum(grades_input)
+  average = sum_of_grades / float(len(grades_input))
+  return average
+
+def grades_variance(scores):
+    average = grades_average(scores)
+    variance = 0
+    for score in scores:
+        variance += (average - score) ** 2
+    return variance / len(scores)
+
+print(grades_variance(grades))
